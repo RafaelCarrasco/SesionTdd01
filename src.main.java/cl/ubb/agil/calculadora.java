4ea0888ab70645743d390845f5cc92365edda699
RafaelCarrasco <r.carrasco.b91@gmail.com>
2016-04-27 12:00:26 -0300

    Test DosMasTres

--- src.main.java/cl/ubb/agil/calculadora.java
+++ src.main.java/cl/ubb/agil/calculadora.java
@@ -1,5 +1,13 @@
 package cl.ubb.agil;
 
 public class calculadora {
-
+	
+	public calculadora(){
+		
+	}
+	public int suma(int num1, int num2){
+		return 5;
+	}
+	
 }
+
--- src.test.java/cl/ubb/agil/calculadoraTest.java
+++ src.test.java/cl/ubb/agil/calculadoraTest.java
@@ -1,5 +1,19 @@
 package cl.ubb.agil;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cl.ubb.agil.calculadora;
+
 public class calculadoraTest {
+	
+	@Test
+	public void DosMasTres(){
+		calculadora cal=new calculadora();
+		int resultado = cal.suma(2, 3);
+		assertThat(resultado,is(5));
+	}
 
 }
